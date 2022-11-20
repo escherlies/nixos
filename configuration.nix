68bbc62341a829699c297c6e3292b8a98068b1cc
Enrico Scherlies <e.scherlies@pm.me>
2022-11-20 19:14:10 +0100

    Add nvim

--- configuration.nix
+++ configuration.nix
@@ -5,16 +5,21 @@
   ];
 
   boot.cleanTmpDir = true;
+
   zramSwap.enable = true;
+
   networking.hostName = "nixe";
+
   services.openssh.enable = true;
   services.openssh.passwordAuthentication = false;
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico1"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINFV5Soberv3DZBGXE3RcIs+DAOMn+yWrzSXUAqjT4r enrico2"
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   system.stateVersion = "22.11";
 
   environment.systemPackages = with pkgs; [
@@ -25,6 +30,20 @@
 
   ];
 
-  programs.fish.enable = true;
+  programs = {
+    fish.enable = true;
+
+    neovim.enable = true;
+    neovim.viAlias = true;
+    neovim.vimAlias = true;
+    neovim.extraConfig = ''
+      set number
+      set relativenumber
+    '';
+
+  };
+
+  environment.variables.EDITOR = "nvim";
+
   users.defaultUserShell = pkgs.fish;
 }
