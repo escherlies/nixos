b3e15e5b07a835e7764bfe256c013d67c442aa33
Enrico Scherlies <e.scherlies@pm.me>
2023-03-11 22:44:25 +0100

    Add graphana

--- configs/prometheus.nix
+++ configs/prometheus.nix
@@ -2,20 +2,46 @@
 
 {
   services = {
-    prometheus = {
+    # grafana configuration
+    grafana = {
       enable = true;
+      port = 2342;
+      addr = "127.0.0.1";
+    };
+
+    # nginx reverse proxy
+    nginx.virtualHosts."static.156.163.90.157.clients.your-server.de" = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+      };
+    };
+
+    services.prometheus = {
+      enable = true;
+      port = 9001;
+
+      exporters = {
+        node = {
+          enable = true;
+          enabledCollectors = [ "systemd" ];
+          port = 9002;
+        };
+      };
+
       scrapeConfigs = [{
-        job_name = "node";
-        scrape_interval = "15s";
-        static_configs = [{ targets = [ "localhost:9100" ]; }];
+        job_name = "chrysalis";
+        static_configs = [{
+          targets = [
+            "127.0.0.1:${
+              toString config.services.prometheus.exporters.node.port
+            }"
+          ];
+        }];
       }];
     };
+
   };
 
-  networking = {
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [ 9100 9090 ];
-    };
-  };
 }
+
