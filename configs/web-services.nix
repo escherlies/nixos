058fffa4d27533f6fba902c6f8539f7a71bfe3a5
Enrico Scherlies <e.scherlies@pm.me>
2024-08-07 11:24:32 +0200

    Add nononote.ai

--- configs/web-services.nix
+++ configs/web-services.nix
@@ -1,6 +1,9 @@
 {
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.hostName = "nixe";
+
+  # Plain websites
+
   security.acme.defaults.email = "escherlies@pm.me";
   security.acme.acceptTerms = true;
   services.nginx.enable = true;
@@ -30,4 +33,43 @@
     enableACME = true;
     root = "/var/www/blog";
   };
+
+  # Docker services
+
+  virtualisation.docker.enable = true;
+
+  environment.systemPackages = with pkgs;
+    [
+      docker-compose
+
+    ];
+
+  users.users.root.extraGroups = [ "docker" ];
+
+  # Use NGINX as reverse proxy
+
+  services.nginx.recommendedProxySettings = true;
+  services.nginx.recommendedTlsSettings = true;
+
+  # Nononote
+  services.nginx.virtualHosts."nononote.ai" = {
+    enableACME = true;
+    forceSSL = true;
+    root = "/var/www/nononote";
+    locations."/api" = {
+      proxyPass = "http://127.0.0.1:8000/api";
+      proxyWebsockets = true;
+      extraConfig =
+        # required when the server wants to use HTTP Authentication
+        "proxy_pass_header Authorization;";
+    };
+    locations."/auth" = {
+      proxyPass = "http://127.0.0.1:8000/auth";
+      proxyWebsockets = false;
+      extraConfig =
+        # required when the server wants to use HTTP Authentication
+        "proxy_pass_header Authorization;";
+    };
+  };
+
 }
