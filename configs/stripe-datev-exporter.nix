07c8428e9f953ea1af8c1fffe9e8ed5087bd0d21
Enrico Scherlies <e.scherlies@pm.me>
2024-09-06 16:07:47 +0200

    Add stripe-datev-exporter

---
+++ configs/stripe-datev-exporter.nix
@@ -0,0 +1,40 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  # Shorter name to access final settings a 
+  # user of stripe-datev-exporter.nix module HAS ACTUALLY SET.
+  # cfg is a typical convention.
+  cfg = config.services.stripe-datev-exporter;
+in {
+  # Declare what settings a user of this "stripe-datev-exporter.nix" module CAN SET.
+  options.services.stripe-datev-exporter = {
+    enable = mkEnableOption "stripe-datev-exporter service";
+  };
+
+  config = mkIf cfg.enable {
+
+    # Invoices
+    systemd.timers."stripe-datev-exporter" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "*-*-01 12:00:00";
+        Unit = "stripe-datev-exporter.service";
+      };
+    };
+
+    systemd.services."stripe-datev-exporter" = {
+      script = ''
+        set -eu
+        ${pkgs.docker}/bin/docker run --env-file /root/.data/stripe-datev-exporter.env ghcr.io/binaryplease/stripe-datev-exporter/app:latest /bin/bash -c "just run"
+      '';
+      requires = [
+        "docker.service"
+
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+    };
+  };
+}
