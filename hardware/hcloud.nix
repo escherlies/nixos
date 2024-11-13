43bd833223cae963bba91a4e33b8b4631325fe6a
Enrico Scherlies <e.scherlies@pm.me>
2024-11-13 16:29:14 +0100

    Test

--- hardware/hcloud.nix
+++ hardware/hcloud.nix
@@ -1,9 +1,22 @@
-{ modulesPath, ... }: {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+{ modulesPath, ... }:
+{
+  boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "vmw_pvscsi" "xen_blkfront" ];
-  boot.initrd.kernelModules = [ "nvme" ];
+
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "vmw_pvscsi"
+    "xen_blkfront"
+    "ahci"
+    "xhci_pci"
+    "virtio_pci"
+    "virtio_scsi"
+    "sd_mod"
+    "sr_mod"
+    "ext4"
+  ];
+
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
