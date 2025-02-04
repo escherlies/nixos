b0a49004f970f2a0e3704763cc683e9b1e16635d
Enrico Scherlies <e.scherlies@pm.me>
2025-02-04 20:28:46 +0100

    Disable ssh pw auth

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -110,6 +110,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
