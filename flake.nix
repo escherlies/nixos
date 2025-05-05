0bf522b97f2744f082192dab801ef3ffccc0beb5
Enrico <e.scherlies@pm.me>
2025-05-05 12:06:35 +0200

    Add home manger

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
   };
 
   outputs =
