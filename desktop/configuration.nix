700e8f3be3b12bfc89f580f733557ba965f3375d
Enrico <e.scherlies@pm.me>
2025-05-05 12:06:35 +0200

    Add home manger

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -85,7 +85,7 @@
     description = "enrico";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-      kdePackages.kate
+      # kdePackages.kate
       #  thunderbird
     ];
   };
@@ -137,6 +137,7 @@
     neofetch
     ctop
     bottom
+    vscode
 
   ];
 
@@ -157,10 +158,6 @@
 
   users.defaultUserShell = pkgs.fish;
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico1"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINFV5Soberv3DZBGXE3RcIs+DAOMn+yWrzSXUAqjT4r enrico2"
-  ];
 
   users.users.enrico.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico1"
