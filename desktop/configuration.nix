71e100acfd9894efd995fa7331e7095ce65d1ef9
Enrico <e.scherlies@pm.me>
2025-05-14 23:31:39 +0200

    Revert "Switch to gnome"
    This reverts commit 5b02a0bf1287542f57df0b20ce5d17b25e0ec394.

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -42,12 +42,13 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # GNOME
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
