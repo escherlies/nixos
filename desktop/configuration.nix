579049127aabb940e538ca350f874e794093ed26
Enrico <e.scherlies@pm.me>
2025-05-14 21:56:48 +0200

    Switch to gnome

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -42,13 +42,12 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # GNOME
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
