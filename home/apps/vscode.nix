ce756d4ea002f642628f8797fae3af0d88b194a1
Enrico Scherlies <e.scherlies@pm.me>
2025-05-28 16:40:26 +0200

    Temporarily manage vscode extensions with script
    - Install extensions using a new justfile command
    - Store extension list in a separate ext.txt file
    - Temporarily disable Nix-managed extensions

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -1,49 +1,58 @@
 { pkgs, ... }:
 {
   programs.vscode.enable = true;
-  programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
-    # Languages
-    jnoortheen.nix-ide
-    nefrob.vscode-just-syntax
-    elmtooling.elm-ls-vscode
-    redhat.vscode-yaml
-    bradlc.vscode-tailwindcss
 
-    # Tooling
-    usernamehw.errorlens
-    eamodio.gitlens
+  # TODO Fix extensions
+  # - https://github.com/nix-community/nix-vscode-extensions
+  # - https://github.com/arvigeus/nixos-config/blob/master/apps/vscode.nix
 
-    # Themes
-    catppuccin.catppuccin-vsc
+  # programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
+  #   # Languages
+  #   jnoortheen.nix-ide
+  #   nefrob.vscode-just-syntax
+  #   elmtooling.elm-ls-vscode
+  #   redhat.vscode-yaml
+  #   bradlc.vscode-tailwindcss
 
-    # Markdown
-    yzhang.markdown-all-in-one
-    yzane.markdown-pdf
-    bierner.markdown-mermaid
-    bierner.markdown-preview-github-styles
+  #   # Tooling
+  #   usernamehw.errorlens
+  #   eamodio.gitlens
 
-    # Agents
-    saoudrizwan.claude-dev
-    rooveterinaryinc.roo-cline
+  #   # Themes
+  #   catppuccin.catppuccin-vsc
 
-    # Misc
-    adpyke.codesnap
+  #   # Markdown
+  #   yzhang.markdown-all-in-one
+  #   yzane.markdown-pdf
+  #   bierner.markdown-mermaid
+  #   bierner.markdown-preview-github-styles
 
-    # Dependencies
-    hbenl.vscode-test-explorer # Elm
-    ms-vscode.test-adapter-converter # Elm
+  #   # Agents
+  #   saoudrizwan.claude-dev
+  #   rooveterinaryinc.roo-cline
 
-    aaron-bond.better-comments
+  #   # Misc
+  #   adpyke.codesnap
 
-    # TODO: Add to nixpkgs https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/editors/vscode/extensions
-    # jameslan.yaclock
-    # gxl.git-graph-3
-    # alekangelov.alek-kai-theme
-    # mathematic.vscode-pdf
-    # liangqin.quick-notes
-    # manuthebyte.dynamic-icon-theme
-    # chunsen.bracket-select
-    # sitoi.ai-commit
-    # pkief.markdown-checkbox
-  ];
+  #   # Dependencies
+  #   hbenl.vscode-test-explorer # Elm
+  #   ms-vscode.test-adapter-converter # Elm
+
+  #   aaron-bond.better-comments
+  #   catppuccin.catppuccin-vsc-icons
+  #   assisrmatheus.sidebar-markdown-notes
+  #   biomejs.biome
+  #   matthewpi.caddyfile-support
+
+  #   # TODO: Add to nixpkgs https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/editors/vscode/extensions
+  #   # jameslan.yaclock
+  #   # gxl.git-graph-3
+  #   # alekangelov.alek-kai-theme
+  #   # mathematic.vscode-pdf
+  #   # liangqin.quick-notes
+  #   # manuthebyte.dynamic-icon-theme
+  #   # chunsen.bracket-select
+  #   # sitoi.ai-commit
+  #   # pkief.markdown-checkbox
+  # ];
 }
