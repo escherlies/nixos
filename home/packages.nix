ea36e810daa01f8ac5166fb9eb8dc15a26b743b0
Enrico Scherlies <e.scherlies@pm.me>
2025-05-28 15:40:16 +0200

    Add sgpt

--- home/packages.nix
+++ home/packages.nix
@@ -19,6 +19,7 @@
     yt-dlp # Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)
     scdl # Download Music from Soundcloud
 
+    shell-gpt
   ];
 
   services.nextcloud-client.enable = true;
