3d7ad79c74a964bfc226569d944614bf111afe09
Enrico Scherlies <e.scherlies@pm.me>
2025-05-30 18:02:45 +0200

    Update keys

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,8 +7,8 @@
   services.openssh.settings.PasswordAuthentication = false;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico1"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINFV5Soberv3DZBGXE3RcIs+DAOMn+yWrzSXUAqjT4r enrico2"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@nixos"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico@macbook"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6otvUYmTVJbNBQylV8kBtHSS4AUVQcN68xZZDowpxR enrico@thinkpad"
   ];
 }
