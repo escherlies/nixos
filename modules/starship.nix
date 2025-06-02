13381b5729e2ef8a3b790124d9559264ad466d08
Enrico Scherlies <e.scherlies@pm.me>
2025-06-03 00:47:52 +0200

    Add nix_shell prompt configuration and update language list in starship.nix

--- modules/starship.nix
+++ modules/starship.nix
@@ -14,12 +14,18 @@
         add_newline = true;
         cmd_duration.show_notifications = false;
 
+        nix_shell = {
+          format = "[  $name](fg:green bg:sapphire)";
+        };
+
         format =
           let
-            languages = "$c$rust$golang$nodejs$php$java$kotlin$haskell$python";
+            languages = "$elm$purescript$bun$deno$rust$golang$nodejs$haskell";
+            # sections = lib.strings.concatStrings [
+            # ]
           in
           ''
-            ┌[](red)$os$username[](bg:peach fg:red)$directory[](bg:yellow fg:peach)$git_branch$git_status[](fg:yellow bg:green)${languages}[](fg:green bg:sapphire)$conda[](fg:sapphire bg:lavender)$time[](fg:lavender) $cmd_duration
+            ┌[](red)$os$username[](bg:peach fg:red)$directory[](bg:yellow fg:peach)$git_branch$git_status[](fg:yellow bg:green)${languages}[](fg:green bg:sapphire)$nix_shell[](fg:sapphire bg:lavender)$time[](fg:lavender) $cmd_duration
             └─$character'';
         # Example: change prompt symbol
         character = {
