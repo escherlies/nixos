eda0bb369a82a288e2fd3fdd2ef9885fc3a03055
Enrico Scherlies <e.scherlies@pm.me>
2025-06-13 17:52:17 +0200

    Add additional SSH keys for user enrico

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -11,4 +11,10 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@desktop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6otvUYmTVJbNBQylV8kBtHSS4AUVQcN68xZZDowpxR enrico@thinkpad"
   ];
+
+  users.users.enrico.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico@macbook"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6otvUYmTVJbNBQylV8kBtHSS4AUVQcN68xZZDowpxR enrico@thinkpad"
+  ];
 }
