b5b2d58275456b6864c644b13e6808d8fca622df
Enrico Scherlies <e.scherlies@pm.me>
2025-09-10 01:13:10 +0200

    Shhhh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -10,11 +10,13 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico@macbook"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@desktop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6otvUYmTVJbNBQylV8kBtHSS4AUVQcN68xZZDowpxR enrico@thinkpad"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIsi0Q1fnYWAmJwPT/FeNqShZgn4z/23APCpazZmTcQ enrico@framework"
   ];
 
   users.users.enrico.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGOkHM4m0DhxJCGH4lkSaaun5RYXZg91LAO15RPeXyS enrico@macbook"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3YsBfgCcmAN3/IBUZBnSVtHa8C/Rx69u46ckegbiHK enrico@desktop"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6otvUYmTVJbNBQylV8kBtHSS4AUVQcN68xZZDowpxR enrico@thinkpad"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIsi0Q1fnYWAmJwPT/FeNqShZgn4z/23APCpazZmTcQ enrico@framework"
   ];
 }
