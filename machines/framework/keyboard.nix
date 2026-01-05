441680f47ae61a56fab4c3be479201bee8ee465c
Enrico Scherlies <e.scherlies@pm.me>
2026-01-05 10:37:14 +0100

    refactor: enhance keyboard settings in keyd configuration

--- machines/framework/keyboard.nix
+++ machines/framework/keyboard.nix
@@ -1,38 +1,49 @@
 { config, pkgs, ... }:
 
 {
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      framework = {
-        ids = [ "0001:0001" ];
-        settings = {
-          main = {
-            capslock = "backspace";
-            leftalt = "leftcontrol";
-            leftcontrol = "leftmeta";
-            leftmeta = "leftalt";
-            # rightalt = "layer(nav)";
-          };
 
-          # nav = {
-          #   h = "left";
-          #   j = "down";
-          #   k = "up";
-          #   l = "right";
-          # };
-        };
+  services.keyd =
+    let
+      common = {
+        leftalt.backspace = "delete";
       };
+    in
+    {
+      enable = true;
+      keyboards = {
+        framework = {
+          ids = [ "0001:0001" ];
+          settings = {
+            main = {
+              capslock = "overload(nav, backspace)";
+              space = "overload(nav, space)";
+              leftalt = "leftcontrol";
+              leftcontrol = "leftmeta";
+              leftmeta = "leftalt";
+              rightcontrol = "rightalt";
+              # rightalt = "layer(nav)";
+            };
 
-      nyquist = {
-        ids = [ "cb10:3156" ];
-        settings = {
-          main = {
-            leftcontrol = "leftmeta";
-            leftmeta = "leftcontrol";
-          };
+            nav = {
+              h = "left";
+              j = "down";
+              k = "up";
+              l = "right";
+            };
+          }
+          // common;
+        };
+
+        nyquist = {
+          ids = [ "cb10:3156" ];
+          settings = {
+            main = {
+              leftcontrol = "leftmeta";
+              leftmeta = "leftcontrol";
+            };
+          }
+          // common;
         };
       };
     };
-  };
 }
