0c59c1a7faddbf337d0edd1e7f8975751ba8b1a0
Enrico Scherlies <e.scherlies@pm.me>
2026-02-11 12:31:20 +0100

    feat: add framework-tool for hardware diagnostics in system packages

--- configs/graphical.nix
+++ configs/graphical.nix
@@ -14,4 +14,7 @@
 
   hardware.i2c.enable = true; # https://wiki.nixos.org/wiki/Backlight#Via_ddcutil
   users.users.enrico.extraGroups = [ "i2c" ];
+
+  # Force Electron apps to use native Wayland instead of XWayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
