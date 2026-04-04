a80fcc6cabb6d44e215955ef253d24d3a0a4f3cf
Enrico Scherlies <e.scherlies@pm.me>
2026-04-04 20:40:50 +0200

    feat(zed): add MIME type associations and editor env vars for framework host

--- home/apps/zed.nix
+++ home/apps/zed.nix
@@ -1,4 +1,61 @@
+{ lib, osConfig, ... }:
+let
+  isFramework = osConfig.networking.hostName == "framework";
+  zedDesktopEntry = "dev.zed.Zed.desktop";
+  sourceMimeTypes = [
+    "text/plain"
+    "text/markdown"
+    "text/x-python"
+    "text/x-shellscript"
+    "text/x-c"
+    "text/x-c++src"
+    "text/x-csharp"
+    "text/x-java"
+    "text/x-go"
+    "text/x-rust"
+    "text/x-php"
+    "text/x-typescript"
+    "text/x-java-source"
+    "text/x-kotlin"
+    "text/x-scala"
+    "text/x-lua"
+    "text/x-ruby"
+    "text/x-swift"
+    "text/x-dart"
+    "text/x-nix"
+    "text/javascript"
+    "text/jsx"
+    "text/tsx"
+    "application/javascript"
+    "application/json"
+    "application/toml"
+    "text/x-toml"
+    "application/xml"
+    "text/html"
+    "text/css"
+    "text/x-sql"
+    "application/x-yaml"
+    "text/x-yaml"
+  ];
+in
 {
   programs.zed-editor.enable = true;
 
+  home.sessionVariables = lib.mkIf isFramework {
+    EDITOR = "zeditor";
+    VISUAL = "zeditor";
+  };
+
+  xdg.mimeApps = lib.mkIf isFramework {
+    enable = true;
+    defaultApplications = builtins.listToAttrs (
+      map (mime: {
+        name = mime;
+        value = [
+          zedDesktopEntry
+          "zed.desktop"
+        ];
+      }) sourceMimeTypes
+    );
+  };
 }
