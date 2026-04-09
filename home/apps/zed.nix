c2b5e8d471e32fc5e2eadae35d1fef40c9e4c1fa
Enrico Scherlies <e.scherlies@pm.me>
2026-04-09 20:15:17 +0200

    remove framework-specific editor env vars from zed config

--- home/apps/zed.nix
+++ home/apps/zed.nix
@@ -1,12 +1,4 @@
 { lib, osConfig, ... }:
-let
-  isFramework = osConfig.networking.hostName == "framework";
-in
 {
   programs.zed-editor.enable = true;
-
-  home.sessionVariables = lib.mkIf isFramework {
-    EDITOR = "zeditor";
-    VISUAL = "zeditor";
-  };
 }
