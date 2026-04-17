5e8482dfb2e0a7798659d8e1c39640580a9e0617
Enrico Scherlies <e.scherlies@pm.me>
2026-04-17 22:44:45 +0200

    Update fresh EDITOR config to use --no-restore

--- modules/editors/fresh.nix
+++ modules/editors/fresh.nix
@@ -3,5 +3,5 @@
 {
   environment.systemPackages = [ inputs.fresh.packages.${pkgs.system}.default ];
 
-  environment.variables.EDITOR = "fresh";
+  environment.variables.EDITOR = "fresh --no-restore";
 }
