309d7c6f5acaaed1655ff490f0a01296726a7d3e
Enrico Scherlies <e.scherlies@pm.me>
2026-05-07 11:55:54 +0200

    chore(shell): remove unused fish functions, aliases, and abbreviations

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -38,25 +38,10 @@ in
         onEvent = "fish_command_not_found";
       };
 
-      # Create and change to a directory
-      mkdircd = ''mkdir -p -- "$argv[1]" && cd -- "$argv[1]"'';
-
       fish_greeting = "";
-
-      ai_commit =
-        let
-          prompt = builtins.readFile ../../config/commit.md;
-        in
-        "git diff --staged | aichat -c ${builtins.toJSON prompt} | copy";
-
-      dev = builtins.readFile ./fish-functions/dev.fish;
     };
 
     shellAbbrs = {
-
-      o = "open";
-      q = "exit";
-      v = "hx";
     };
 
     shellAliases = rec {
@@ -70,28 +55,12 @@ in
       la = "${ls} -lbhgmua@ --color-scale --git";
       lt = "${ls} --tree --level=2";
 
-      "!" = "just";
-
-      # Git
-      gs = "${pkgs.git}/bin/git status";
-
-      ga = "${pkgs.git}/bin/git add .";
-      # git diff --no-ext-diff
-
       # Other
       lsblk = "lsblk -o name,mountpoint,label,size,type,uuid";
 
       qr = "${pkgs.qrencode}/bin/qrencode -t utf8 -o-";
 
       weather = "${pkgs.curl}/bin/curl -4 http://wttr.in/Berlin";
-
-      zzz = "systemctl suspend";
-
-      copy = "wl-copy";
-
-      nd = "nix develop -c $SHELL";
-      day = "lookandfeeltool -a org.kde.breeze.desktop";
-      night = "lookandfeeltool -a org.kde.breezedark.desktop";
 
       npx = "bun x";
     };
